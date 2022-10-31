ea74cc1666e8f6170a448107cb2eb1a777281210
Valiantsin Shkodzin <95443499+ValiantsinShkodzin@users.noreply.github.com>
2022-10-31 16:28:38 +0300

    Users API_axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,14 +1,23 @@
+import axios from "axios";
 import React from 'react';
-import classes from './Users.module.css'
+import classes from './Users.module.css';
+import userPhoto from '../../img/userPhoto.png'
 
 function Users(props) {
+    if (props.users.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then (response => {
+        debugger;    
+        props.setUsers(response.data.items);
+        });
+    }
+
     return <div>
         {
             props.users.map(u =>
         <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photoUrl} className={classes.userPhoto}></img>
+                    <img src={u.photos.small != null ? u.photos.small: userPhoto} className={classes.userPhoto}></img>
                 </div>
                 <div>
                     {u.followed ? <button onClick={() => {props.unfollow(u.id)}}>UNFOLLOW</button>:<button onClick={() => {props.follow(u.id)}}>FOLLOW</button>}
@@ -16,12 +25,12 @@ function Users(props) {
             </span>
             <span>
                 <span>
-                    <div>{u.fullName}</div>
+                    <div>{u.name}</div>
                     <div>{u.status}</div>
                 </span>
                 <span>
-                    <div>{u.location.country}</div>
-                    <div>{u.location.city}</div>
+                    <div>{"u.location.country"}</div>
+                    <div>{"u.location.city"}</div>
                 </span>
             </span>
         </div>)
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -4,14 +4,14 @@ const SET_USERS = "SET-USERS"
 
 let initialState = {
     users: [
-        { id: 1, photoUrl:'https://damion.club/uploads/posts/2022-03/1646980611_1-damion-club-p-portreti-devushek-krasivie-foto-1.jpg',
-            followed: true, fullName: 'Valero', status: 'Big boss', location:{city:'Minsk', country:'Belarus'} },
-        { id: 2, photoUrl:'https://images.pexels.com/photos/14043926/pexels-photo-14043926.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            followed: false,fullName: 'Valeria', status: 'Pretty woman', location:{city:'Warszava', country:'Rzecz Pospolita'} },
-        { id: 3, photoUrl:'https://images.pexels.com/photos/14059376/pexels-photo-14059376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            followed: true,fullName: 'Victotia', status: 'Sexy girl', location:{city:'Berlin', country:'Germany'} },
-        { id: 4, photoUrl:'https://images.pexels.com/photos/13188542/pexels-photo-13188542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            followed: false,fullName: 'Verginia', status: 'Sweet chika', location:{city:'Budapest', country:'Hungary'} },
+        // { id: 1, photoUrl:'https://damion.club/uploads/posts/2022-03/1646980611_1-damion-club-p-portreti-devushek-krasivie-foto-1.jpg',
+        //     followed: true, fullName: 'Valero', status: 'Big boss', location:{city:'Minsk', country:'Belarus'} },
+        // { id: 2, photoUrl:'https://images.pexels.com/photos/14043926/pexels-photo-14043926.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        //     followed: false,fullName: 'Valeria', status: 'Pretty woman', location:{city:'Warszava', country:'Rzecz Pospolita'} },
+        // { id: 3, photoUrl:'https://images.pexels.com/photos/14059376/pexels-photo-14059376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        //     followed: true,fullName: 'Victotia', status: 'Sexy girl', location:{city:'Berlin', country:'Germany'} },
+        // { id: 4, photoUrl:'https://images.pexels.com/photos/13188542/pexels-photo-13188542.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        //     followed: false,fullName: 'Verginia', status: 'Sweet chika', location:{city:'Budapest', country:'Hungary'} },
     ],
 };
 
