e6de0edc7ef39297e460c8ebf3fc19a13e0b0df8
Valiantsin Shkodzin <95443499+ValiantsinShkodzin@users.noreply.github.com>
2022-11-25 16:29:10 +0300

    Redux-thunk

--- src/api/api.js
+++ src/api/api.js
@@ -20,7 +20,14 @@ export const usersAPI = {
     },
     unfollow(userId) {
         return instance.delete(`follow/${userId}`)
-    }
+    },
+    getProfile(userId) {
+        return instance.get(`profile/${userId}`)
+        }
+}
+
+export const authAPI = {
+   
 }
 
 
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { connect } from 'react-redux';
 import { setUserProfile } from '../../redux/profileReducer';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { usersAPI } from '../../api/api';
 
 class ProfileContainer extends React.Component {
     componentDidMount(){
@@ -11,10 +12,9 @@ class ProfileContainer extends React.Component {
         if (!userId) {
             userId = 2;
         }
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
-            .then(response => {
-                this.props.setUserProfile(response.data);
-            });
+        usersAPI.getProfile(userId).then(response => {
+            this.props.setUserProfile (response.data);
+        });
     }
 
     componentDidUpdate(prevProps){
