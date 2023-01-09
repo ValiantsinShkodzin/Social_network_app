23c58902b33eec44ec902d1ccecd65fbcab6e238
Valiantsin Shkodzin <95443499+ValiantsinShkodzin@users.noreply.github.com>
2023-01-09 16:39:57 +0300

    LocalState (setState)

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -42,6 +42,6 @@ let mapStateToProps = (state) => ({
 export default compose(
     connect (mapStateToProps, {getUserProfile}),
     withRouter,
-    withAuthRedirect
+    // withAuthRedirect
     )(ProfileContainer);
 
---
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class ProfileStatus extends React.Component {
+    //локальный state компоненты
+    state = {
+        editMode: true
+    }
+    activateEditMode() {
+        this.setState({
+            editMode: true
+        });
+    }
+    deactivateEditMode() {
+        this.setState({
+            editMode: false
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                {!this.state.editMode &&
+                    <div>
+                        <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                    </div>}
+                {this.state.editMode &&
+                    <div>
+                        <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} type="text" value={this.props.status} />
+                    </div>}
+            </div>
+        )
+    }
+}
+
+export default ProfileStatus;
